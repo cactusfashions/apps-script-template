f9efbfcca4a885881f7226fb6cd3c4b6691295bd
Cactus Fashions <mis1@cactusfashions.com>
2025-06-17 14:41:10 +0530

    Create ScriptError.js

---
+++ ScriptError.js
@@ -0,0 +1,11 @@
+// Standardized error object for Apps Script
+class ScriptError extends Error {
+    constructor(statusCode, message = "Something went wrong", errors = [], stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.stack = stack || new Error().stack;
+        this.success = false;
+    }
+}
